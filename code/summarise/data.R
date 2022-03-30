78a239433ed263d5980479f4b37e70e42a9fe0d5
Kath Sherratt <62290797+kathsherratt@users.noreply.github.com>
2022-03-30 18:49:30 +0100

    add SI

--- code/summarise/data.R
+++ code/summarise/data.R
@@ -2,8 +2,6 @@
 #   and figures for supplementary information (SI)
 library(dplyr)
 library(lubridate)
-library(ggplot2)
-library(patchwork)
 
 # Get evaluation scores if not already present
 if (!exists("scores_model")) {source(here("code", "load", "evaluation-scores.R"))}
@@ -73,42 +71,3 @@ hub_scores <- list(
               p_better = round(sum(rel_wis_distance <= 0) / n * 100)) %>%
     split(.$target_variable)
 )
-
-# SI figure 1: heatmap of participation across targets -----------------------
-"si_fig1" = models_per_target %>%
-  ggplot(aes(x = horizon, y = location_name, fill = n_scores)) +
-  geom_tile() +
-  scale_fill_viridis_c(direction = -1) +
-  labs(x = "Horizon", y = NULL, fill = "Number of forecasts",
-       subtitle = "Total number of forecasts included in evaluation, \n by target location, week ahead horizon, and variable") +
-  facet_wrap("target_variable") +
-  theme(legend.position = "bottom")
-
-# ggsave("output/figures/si-figure-1.png", height = 5, width = 5)
-
-# SI fig 2 ----------------------------------------------------------------
-# Distribution of non-hub model scores
-si_fig2a <- scores_model_exhub %>%
-  filter(rel_wis <= 3) %>%
-  ggplot(aes(x = rel_wis)) +
-  geom_histogram(binwidth = 0.1) +
-  geom_vline(aes(xintercept = 1), lty = 2) +
-  labs(x = "Scaled relative WIS", y = "Frequency",
-       subtitle = "(a) Contributed forecasts")
-
-# Distance between model score and ensemble score
-si_fig2b <- scores_model_exhub %>%
-  filter(rel_wis <= 3) %>%
-  ggplot(aes(x = rel_wis_distance)) +
-  geom_histogram(binwidth = 0.1) + # 10% difference from ensemble score
-  geom_vline(aes(xintercept = 0), lty = 2) +
-  labs(x = "Difference from ensemble",
-       y = NULL,
-       subtitle = "(b) Contributed forecasts \n compared to the Hub ensemble model")
-
-si_figure_2 <- si_fig2a + si_fig2b + plot_layout(nrow = 1) +
-  plot_annotation(
-    subtitle = "Comparison of scores between participating model forecasts \n and Hub ensemble of all available forecasts for each target",
-    caption = paste("N =", modeller_scores$n_rel_wis - modeller_scores$score_3, "excluding", modeller_scores$score_3, "scores with scaled relative WIS > 3"))
-
-# ggsave("output/figures/si-figure-2.png", height = 3, width = 7)
