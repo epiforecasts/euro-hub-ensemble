8a86212be4c55bcc851ca83ca41188158c0c9f97
Kath Sherratt <62290797+kathsherratt@users.noreply.github.com>
2023-01-18 18:22:48 +0000

    team metadata as csv file

--- code/summarise/team-metadata.R
+++ code/summarise/team-metadata.R
@@ -1,4 +1,4 @@
-# Supplementary information
+# Supplementary information: team metadata
 library(here)
 library(dplyr)
 library(ggplot2)
@@ -6,17 +6,12 @@ library(patchwork)
 library(yaml)
 library(purrr)
 library(fs)
-library(knitr)
-library(kableExtra)
+source(here("code", "load", "evaluation-scores.R"))
 
 # Participating teams -----------------------------------------------------
 
 # Note: the below code written by Hugo Gruson and Seb Funk for the Hub website:
 #  See https://github.com/covid19-forecast-hub-europe/covid19-forecast-hub-europe-website/blob/main/community.Rmd
-
-github_repo <- "epiforecasts/covid19-forecast-hub-europe"
-branch <- "main"
-
 
 github_repo <- "epiforecasts/covid19-forecast-hub-europe"
 branch <- "main"
@@ -36,31 +31,22 @@ team_df <-
   set_names() %>%
   map(read_yaml) %>%
   imap_dfr(~ list(
-    link = .y,
-    authors = glue::glue_collapse(purrr::map(.x$model_contributors, "name"), sep = ", "),
-    model_abbr = .x$model_abbr,
-    website_url = .x$website_url,
-    team_name = .x$team_name,
-    methods = .x$methods,
+    Team = .x$team_name,
+    Model = .x$model_abbr,
+    Authors = glue::glue_collapse(purrr::map(.x$model_contributors, "name"), sep = ", "),
+    Methods = .x$methods,
+    Website = .x$website_url,
+    Metadata = .y,
     team_model_designation = .x$team_model_designation)) %>%
-  filter(model_abbr %in% scores_model$model &
+  filter(Model %in% scores_model$model &
            !grepl("other", team_model_designation)) %>%
-  arrange(tolower(model_abbr)) %>%
-  mutate(
-    team_name = glue::glue("[{team_name}]({website_url})"),
-    metadata = glue::glue("[{model_abbr}]({link})")) %>%
-  select(authors, team_name, methods, metadata)
-
-team_table <- team_df  %>%
-  relocate(
-    "Team" = team_name,
-    "Authors" = authors,
-    "Methods" = methods,
-    "Metadata" =metadata
-  )
+  arrange(tolower(Model)) %>%
+  select(-team_model_designation)
 
 # Replace a team name on their request
-team_table <- team_table |>
+team_df <- team_df |>
   mutate(Team = gsub("UNIPG_UNIMIB_USI_UNINSUBRIA",
                      "University of Perugia / University of Milano-Bicocca / Università della Svizzera Italiana",
                      Team))
+
+readr::write_excel_csv(team_df, here("output", "metadata", "team-metadata.csv"))
